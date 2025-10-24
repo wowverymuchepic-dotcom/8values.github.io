7fa1efb1f5045055aed5bc60b55209a29d8338bd
wowverymuchepic-dotcom <wowverymuchepic@gmail.com>
2025-10-24 09:57:47 -0400

    Replace existing questions with new religious questions

--- questions.js
+++ questions.js
@@ -1,31 +1,50 @@
 questions = [
     {
-        "question": "penis.",
+        "question": "My religion is the only true one and other religions have no place in society.",
         "effect": {
-            "reli": 10,
+            "reli": -10,
             "govt": 0,
-            "orga": -5,
-            "acce": 0
+            "orga": 0,
+            "acce": -10
         }
     },
     {
-        "question": "It is necessary for the government to intervene in the reliomy to protect consumers.",
+        "question": "My religion should be spread as much as possible.",
+        "effect": {
+            "reli": -10,
+            "govt": 0,
+            "orga": 0,
+            "acce": -10
+        }
+    },
+    {
+        "question": "Children should be educated in only my religion’s values.",
+        "effect": {
+            "reli": -10,
+            "govt": 0,
+            "orga": 0,
+            "acce": -10
+        }
+    },
+    {
+        "question": "We should have less of a focus on religion as a society.",
         "effect": {
             "reli": 10,
-            "govt": 0,
+            "govt": 10,
             "orga": 0,
             "acce": 0
         }
     },
     {
-        "question": "The freer the markets, the freer the people.",
+        "question": "Society has moved past a need for religion.",
         "effect": {
-            "reli": -10,
+            "reli": 10,
             "govt": 0,
             "orga": 0,
             "acce": 0
         }
     },
 ];
+
 
 
