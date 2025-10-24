b3096240b4648309a801e1ea0e055bc9b0b35327
wowverymuchepic-dotcom <wowverymuchepic@gmail.com>
2025-10-24 10:21:26 -0400

    Expand questions on religion and societal roles
    Added new questions related to religion and its societal impact.

--- questions.js
+++ questions.js
@@ -44,7 +44,314 @@ questions = [
             "acce": 0
         }
     },
+    {
+        "question": "Religions should have core central doctrines and hierarchies.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": -10,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religion should play a role in government.",
+        "effect": {
+            "reli": -10,
+            "govt": -10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "I would consider myself religious.",
+        "effect": {
+            "reli": -20,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "The state should mandate atheism.",
+        "effect": {
+            "reli": 10,
+            "govt": 20,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "A God or higher force exists.",
+        "effect": {
+            "reli": -10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religion should be in the hands of the people and not the church.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 10,
+            "acce": 0
+        }
+    },
+    {
+        "question": "The government should not fund religious organizations in any way.",
+        "effect": {
+            "reli": 0,
+            "govt": 10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Laws should be based on religion.",
+        "effect": {
+            "reli": -10,
+            "govt": -10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religion is important to have in society.",
+        "effect": {
+            "reli": -10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Everyone should be able to practice their religion.",
+        "effect": {
+            "reli": 0,
+            "govt": -5,
+            "orga": 0,
+            "acce": 10
+        }
+    },
+    {
+        "question": "Most bad things happening in the world are caused by us turning away from religion.",
+        "effect": {
+            "reli": -10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "People need religious authorities to be able to properly worship.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": -10,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Society has moved past a need for religion.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Having a formal church is oppressive.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 10,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Theocracy is a good form of government.",
+        "effect": {
+            "reli": -10,
+            "govt": -10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religious persecutions such as the inquisition are a good thing for society and my religion.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 0,
+            "acce": -10
+        }
+    },
+    {
+        "question": "It is immoral to force people to convert to a religion.",
+        "effect": {
+            "reli": 0,
+            "govt": 10,
+            "orga": 0,
+            "acce": 10
+        }
+    },
+    {
+        "question": "It is okay for people to take ideas they like from multiple religions and incorporate them into their beliefs.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 10,
+            "acce": 10
+        }
+    },
+    {
+        "question": "Religion is inherently immoral.",
+        "effect": {
+            "reli": 20,
+            "govt": 10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Only one religion should exist.",
+        "effect": {
+            "reli": -10,
+            "govt": -10,
+            "orga": -10,
+            "acce": -20
+        }
+    },
+    {
+        "question": "There is no such thing as heresy.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 20,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Science is a better way of understanding the world than religion.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "I would consider myself an atheist.",
+        "effect": {
+            "reli": 20,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "There must be a separation between church and state.",
+        "effect": {
+            "reli": 0,
+            "govt": 10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "I don’t believe there is something after death.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "I don’t practice a religion.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Faith is complete nonsense.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religion should have more structure and leadership.",
+        "effect": {
+            "reli": 0,
+            "govt": -5,
+            "orga": -10,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Government leaders should be more religious.",
+        "effect": {
+            "reli": 0,
+            "govt": -10,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "It's better for society to have multiple religions than for society to only have one.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 0,
+            "acce": 10
+        }
+    },
+    {
+        "question": "No religion is truer than another.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 0,
+            "acce": 10
+        }
+    },
+    {
+        "question": "All religions have their pros and cons.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 0,
+            "acce": 10
+        }
+    },
+    {
+        "question": "Religion doesn’t bring me comfort.",
+        "effect": {
+            "reli": 10,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "question": "Religion should be more organized.",
+        "effect": {
+            "reli": 0,
+            "govt": 0,
+            "orga": -10,
+            "acce": 0
+        }
+    },
 ];
+
 
 
 
