a77ec962cc8b71ef13a3e20d0632cb2f1a7eeef6
wowverymuchepic-dotcom <wowverymuchepic@gmail.com>
2025-10-24 11:56:10 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -179,7 +179,161 @@ ideologies = [
             "acce": 50
         }
     },
+    {
+        "name": "Moderately Irreligious",
+        "stats": {
+            "reli": 70,
+            "govt": 55,
+            "orga": 55,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Religious",
+        "stats": {
+            "reli": 40,
+            "govt": 55,
+            "orga": 55,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderate",
+        "stats": {
+            "reli": 55,
+            "govt": 55,
+            "orga": 55,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Organized",
+        "stats": {
+            "reli": 55,
+            "govt": 55,
+            "orga": 40,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Unorganized",
+        "stats": {
+            "reli": 55,
+            "govt": 55,
+            "orga": 70,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Theocratic",
+        "stats": {
+            "reli": 55,
+            "govt": 40,
+            "orga": 55,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Secular",
+        "stats": {
+            "reli": 55,
+            "govt": 70,
+            "orga": 55,
+            "acce": 55
+        }
+    },
+    {
+        "name": "Moderately Accepting",
+        "stats": {
+            "reli": 55,
+            "govt": 55,
+            "orga": 55,
+            "acce": 70
+        }
+    },
+    {
+        "name": "Moderately Unaccepting",
+        "stats": {
+            "reli": 55,
+            "govt": 55,
+            "orga": 55,
+            "acce": 40
+        }
+    },
+    {
+        "name": "Geeked",
+        "stats": {
+            "reli": 0,
+            "govt": 100,
+            "orga": 0,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Freaked",
+        "stats": {
+            "reli": 100,
+            "govt": 0,
+            "orga": 100,
+            "acce": 0
+        }
+    },
+    {
+        "name": "Orthodox",
+        "stats": {
+            "reli": 0,
+            "govt": 40,
+            "orga": 10,
+            "acce": 25
+        }
+    },
+    {
+        "name": "Reform",
+        "stats": {
+            "reli": 0,
+            "govt": 70,
+            "orga": 70,
+            "acce": 85
+        }
+    },
+    {
+        "name": "Reconstructionist",
+        "stats": {
+            "reli": 10,
+            "govt": 85,
+            "orga": 85,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Puritan",
+        "stats": {
+            "reli": 10,
+            "govt": 25,
+            "orga": 40,
+            "acce": 10
+        }
+    },
+    {
+        "name": "Pagan",
+        "stats": {
+            "reli": 25,
+            "govt": 85,
+            "orga": 85,
+            "acce": 40
+        }
+    },
+    {
+        "name": "Apolitical Fundamentalist",
+        "stats": {
+            "reli": 0,
+            "govt": 50,
+            "orga": 0,
+            "acce": 0
+        }
+    },
 ];
+
 
 
 
