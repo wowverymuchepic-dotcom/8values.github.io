935b185bb648f37de25c907964db4f79b833add5
wowverymuchepic-dotcom <wowverymuchepic@gmail.com>
2025-10-24 11:36:19 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,472 +1,185 @@
 ideologies = [
     {
-        "name": "Anarcho-ppism",
+        "name": "Zealot",
         "stats": {
-            "reli": 100,
-            "govt": 50,
-            "orga": 100,
-            "acce": 90
+            "reli": 0,
+            "govt": 0,
+            "orga": 0,
+            "acce": 0
         }
     },
     {
-        "name": "LIBERAL Communism",
+        "name": "Secular",
         "stats": {
-            "reli": 100,
-            "govt": 70,
-            "orga": 80,
-            "acce": 80
+            "reli": 85,
+            "govt": 85,
+            "orga": 50,
+            "acce": 50
         }
     },
     {
-        "name": "Trotskyismism",
+        "name": "State Atheist",
         "stats": {
             "reli": 100,
             "govt": 100,
-            "orga": 60,
-            "acce": 80
+            "orga": 50,
+            "acce": 50
         }
     },
     {
-        "name": "No Iphone moment",
+        "name": "Atheist",
         "stats": {
             "reli": 100,
-            "govt": 70,
+            "govt": 85,
+            "orga": 50,
+            "acce": 50
+        }
+    },
+    {
+        "name": "Theocrat",
+        "stats": {
+            "reli": 0,
+            "govt": 0,
             "orga": 40,
-            "acce": 80
+            "acce": 40
         }
     },
     {
-        "name": "Leon :o",
+        "name": "Ecclesiasticist",
         "stats": {
-            "reli": 100,
-            "govt": 30,
-            "orga": 30,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Leninade",
-        "stats": {
-            "reli": 100,
+            "reli": 0,
             "govt": 40,
-            "orga": 20,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Gay",
-        "stats": {
-            "reli": 100,
-            "govt": 20,
             "orga": 0,
-            "acce": 60
+            "acce": 25
         }
     },
     {
-        "name": "I LOVE Communism",
+        "name": "Religious Liberal",
         "stats": {
-            "reli": 100,
+            "reli": 0,
             "govt": 50,
-            "orga": 30,
-            "acce": 30
-        }
-    },
-    {
-        "name": "State Socialty",
-        "stats": {
-            "reli": 80,
-            "govt": 30,
-            "orga": 30,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Theo Socialism",
-        "stats": {
-            "reli": 80,
-            "govt": 50,
-            "orga": 30,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Religous Socialism",
-        "stats": {
-            "reli": 80,
-            "govt": 50,
-            "orga": 70,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Democrats are Socialism",
-        "stats": {
-            "reli": 80,
-            "govt": 50,
-            "orga": 50,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Revolution can't Socialism",
-        "stats": {
-            "reli": 80,
-            "govt": 20,
-            "orga": 50,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Libertarian Socialism",
-        "stats": {
-            "reli": 80,
-            "govt": 80,
-            "orga": 80,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Anarcho-Syndicalism",
-        "stats": {
-            "reli": 80,
-            "govt": 50,
-            "orga": 100,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Left-Wing Populism",
-        "stats": {
-            "reli": 60,
-            "govt": 40,
-            "orga": 30,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Theocratic Distributism",
-        "stats": {
-            "reli": 60,
-            "govt": 40,
-            "orga": 30,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Distributism",
-        "stats": {
-            "reli": 60,
-            "govt": 50,
-            "orga": 50,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Social Liberalism",
-        "stats": {
-            "reli": 60,
-            "govt": 60,
-            "orga": 60,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Christian Democracy",
-        "stats": {
-            "reli": 60,
-            "govt": 60,
-            "orga": 50,
-            "acce": 30
-        }
-    },
-    {
-        "name": "Social Democracy",
-        "stats": {
-            "reli": 60,
-            "govt": 70,
-            "orga": 60,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Progressivism",
-        "stats": {
-            "reli": 60,
-            "govt": 80,
-            "orga": 60,
+            "orga": 85,
             "acce": 100
         }
     },
     {
-        "name": "Anarcho-Mutualism",
+        "name": "Anarchist Theist",
         "stats": {
-            "reli": 60,
+            "reli": 0,
             "govt": 50,
             "orga": 100,
-            "acce": 70
+            "acce": 100
         }
     },
     {
-        "name": "National Totalitarianism",
+        "name": "Spiritual",
         "stats": {
-            "reli": 50,
-            "govt": 20,
-            "orga": 0,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Global Totalitarianism",
-        "stats": {
-            "reli": 50,
-            "govt": 80,
-            "orga": 0,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Technocracy",
-        "stats": {
-            "reli": 60,
-            "govt": 60,
-            "orga": 20,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Centist",
-        "stats": {
-            "reli": 50,
+            "reli": 40,
             "govt": 50,
+            "orga": 100,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Omnist",
+        "stats": {
+            "reli": 0,
+            "govt": 50,
+            "orga": 100,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Fundamentalist",
+        "stats": {
+            "reli": 0,
+            "govt": 25,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "name": "Agnostic",
+        "stats": {
+            "reli": 70,
+            "govt": 85,
             "orga": 50,
             "acce": 50
         }
     },
     {
-        "name": "Liberalism",
+        "name": "Atheist Theocrat",
         "stats": {
-            "reli": 50,
-            "govt": 60,
-            "orga": 60,
-            "acce": 60
-        }
-    },
-    {
-        "name": "Religious Anarchism",
-        "stats": {
-            "reli": 50,
-            "govt": 50,
-            "orga": 100,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Right-Wing Populism",
-        "stats": {
-            "reli": 40,
-            "govt": 30,
-            "orga": 30,
-            "acce": 30
-        }
-    },
-    {
-        "name": "Moderate Conservatism",
-        "stats": {
-            "reli": 40,
-            "govt": 40,
-            "orga": 50,
-            "acce": 30
-        }
-    },
-    {
-        "name": "Reactionary",
-        "stats": {
-            "reli": 40,
-            "govt": 40,
-            "orga": 40,
-            "acce": 10
-        }
-    },
-    {
-        "name": "Social Libertarianism",
-        "stats": {
-            "reli": 60,
-            "govt": 70,
-            "orga": 80,
-            "acce": 70
-        }
-    },
-    {
-        "name": "Libertarianism",
-        "stats": {
-            "reli": 40,
-            "govt": 60,
-            "orga": 80,
-            "acce": 60
-        }
-    },
-    {
-        "name": "Anarcho-Egoism",
-        "stats": {
-            "reli": 40,
-            "govt": 50,
-            "orga": 100,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Nazism",
-        "stats": {
-            "reli": 40,
+            "reli": 100,
             "govt": 0,
             "orga": 0,
-            "acce": 5
+            "acce": 0
         }
     },
     {
-        "name": "Autocracy",
+        "name": "Chauvinist",
+        "stats": {
+            "reli": 0,
+            "govt": 25,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "name": "Unaccepting",
+        "stats": {
+            "reli": 25,
+            "govt": 50,
+            "orga": 25,
+            "acce": 40
+        }
+    },
+    {
+        "name": "Intolerant",
+        "stats": {
+            "reli": 25,
+            "govt": 50,
+            "orga": 0,
+            "acce": 0
+        }
+    },
+    {
+        "name": "Syncretist Theocrat",
+        "stats": {
+            "reli": 0,
+            "govt": 0,
+            "orga": 85,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Conservative",
+        "stats": {
+            "reli": 25,
+            "govt": 40,
+            "orga": 25,
+            "acce": 25
+        }
+    },
+    {
+        "name": "Animist",
+        "stats": {
+            "reli": 0,
+            "govt": 40,
+            "orga": 100,
+            "acce": 100
+        }
+    },
+    {
+        "name": "Centrist",
         "stats": {
             "reli": 50,
-            "govt": 20,
-            "orga": 20,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Fascism",
-        "stats": {
-            "reli": 40,
-            "govt": 20,
-            "orga": 20,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Capitalist Fascism",
-        "stats": {
-            "reli": 20,
-            "govt": 20,
-            "orga": 20,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Conservatism",
-        "stats": {
-            "reli": 30,
-            "govt": 40,
-            "orga": 40,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Neo-Liberalism",
-        "stats": {
-            "reli": 30,
-            "govt": 30,
-            "orga": 50,
-            "acce": 60
-        }
-    },
-    {
-        "name": "Classical Liberalism",
-        "stats": {
-            "reli": 30,
-            "govt": 60,
-            "orga": 60,
-            "acce": 80
-        }
-    },
-    {
-        "name": "Authoritarian Capitalism",
-        "stats": {
-            "reli": 20,
-            "govt": 30,
-            "orga": 20,
-            "acce": 40
-        }
-    },
-    {
-        "name": "State Capitalism",
-        "stats": {
-            "reli": 20,
             "govt": 50,
-            "orga": 30,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Neo-Conservatism",
-        "stats": {
-            "reli": 20,
-            "govt": 20,
-            "orga": 40,
-            "acce": 20
-        }
-    },
-    {
-        "name": "Fundamentalism",
-        "stats": {
-            "reli": 20,
-            "govt": 30,
-            "orga": 30,
-            "acce": 5
-        }
-    },
-    {
-        "name": "Libertarian Capitalism",
-        "stats": {
-            "reli": 20,
-            "govt": 50,
-            "orga": 80,
-            "acce": 60
-        }
-    },
-    {
-        "name": "Market Anarchism",
-        "stats": {
-            "reli": 20,
-            "govt": 50,
-            "orga": 100,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Objectivism",
-        "stats": {
-            "reli": 10,
-            "govt": 50,
-            "orga": 90,
-            "acce": 40
-        }
-    },
-    {
-        "name": "Totalitarian Capitalism",
-        "stats": {
-            "reli": 0,
-            "govt": 30,
-            "orga": 0,
-            "acce": 50
-        }
-    },
-    {
-        "name": "Ultra-Capitalism",
-        "stats": {
-            "reli": 0,
-            "govt": 40,
             "orga": 50,
             "acce": 50
         }
     },
-    {
-        "name": "Anarcho-Capitalism",
-        "stats": {
-            "reli": 0,
-            "govt": 50,
-            "orga": 100,
-            "acce": 50
-        }
-    }
 ];
+
 
 
